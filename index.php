3c2e1a3c79984b4583e37524f181cb1fb75a5346
Hutton Amison-Addy <42409019+Argivist@users.noreply.github.com>
2025-11-26 10:55:33 +0000

    Implement code structure updates and remove redundant code blocks

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+// get system theme
+
+?>
+
+<html>
+
+<head>
+    <title>AMS</title>
+    <link rel="stylesheet" href="assets/css/styles.css" />
+</head>
+
+<body>
+    <div class="body">
+        <div class="main">
+            <img src="panda.png" class="panda" />
+            <h1>Hello</h1>
+            <p>Welcome to the AMS. This platform is still under development.</p>
+        </div>
+        <p> Powered by EdrionTech</p>
+    </div>
+
+
+
+</body>
+
+</html>
